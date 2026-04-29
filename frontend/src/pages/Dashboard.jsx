d76622d39a33941c3e31a5128a6e5347a819ccf7
Sreehari K P <sreehari.kp@gnapi.tech>
2026-04-29 10:54:34 +0530

    Added logout btn and styled

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import "./Dashboard.css"
 
+const logoutUrl = import.meta.env.VITE_LOGOUT;
 function Dashboard() {
+
+  const handleLogout = () => {
+    window.location.href = logoutUrl;
+  }
   return (
-    <div>
-      <h1>Welcome to Dashboard</h1>
+     <div className="dashboard-container">
+     <div className="dashboard-header">
+        <button className="logout-btn" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+    <div className="dashboard-content">
+        <div className="dashboard-card">
+          <h1>Welcome to Dashboard</h1>
+        </div>
+      </div>
+
     </div>
   )
 }
